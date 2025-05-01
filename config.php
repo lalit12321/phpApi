440f7efd57129cc57cb8dc9cdca9732f79d6f782
lalit12321 <136174927+lalit12321@users.noreply.github.com>
2025-05-01 17:39:09 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$connect = mysqli_connect("localhost", "root", "", "student_manajment_system");
+$connect = mysqli_connect("sql111.infinityfree.com", "if0_38796249", "Lalit5400", "if0_38796249_ragistration");
 
 if (!$connect) {
     die(json_encode(array("status" => "error", "message" => "Database connection failed")));
